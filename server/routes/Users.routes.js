af1288bee8218b2392856394938f57ae73723a2c
Amara <70609663+RedSnoww12@users.noreply.github.com>
2022-06-30 21:18:32 +0200

    auth jwt api

--- server/routes/Users.routes.js
+++ server/routes/Users.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import User from './../models/UserModel.js';
+import generateToken from './../utils/generateToken.js';
+import protect from './../Middleware/AuthMiddleware.js';
 
 const userRoute = express.Router();
 
@@ -10,28 +12,65 @@ userRoute.get("/", asyncHandler(async(req, res) => {
     res.json(users);
 }));
 
-// GET ONE SHOE
-userRoute.get("/:id", asyncHandler(async(req, res) => {
-    const user = await User.find({_id: req.params.id});
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404);
-        throw new Error("User not found !");
-    }
-}));
+// GET ONE USER
+// userRoute.get("/:id", asyncHandler(async(req, res) => {
+//     const user = await User.find({_id: req.params.id});
+//     if (user) {
+//         res.json(user);
+//     } else {
+//         res.status(404);
+//         throw new Error("User not found !");
+//     }
+// }));
 
 // LOGIN 
 userRoute.post("/login", asyncHandler(async(req,res) =>{
     const { email, password } = req.body;
-    const user = await User.findOne({email});
+    const user = await User.findOne({mail: email});
     
     if (user && (await user.matchPassword(password))) {
-        res.json(user)
+        res.json({
+            _id: user._id,
+            last_name: user.last_name,
+            first_name: user.first_name,
+            nickname: user.nickname,
+            phone: user.phone,
+            adress: user.adress,
+            mail: user.mail,
+            password: user.password,
+            isAdmin: user.isAdmin,
+            token: generateToken(user.id),
+            createAt: user.createAt
+        })
     }
     else{
         res.status(401)
         throw new Error("Invalid Email or Password");
+    }
+
+}));
+
+// PROFILE 
+userRoute.get("/profile", protect, asyncHandler(async(req,res) =>{
+    
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            last_name: user.last_name,
+            first_name: user.first_name,
+            nickname: user.nickname,
+            phone: user.phone,
+            adress: user.adress,
+            mail: user.mail,
+            password: user.password,
+            isAdmin: user.isAdmin,
+            createAt: user.createAt
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found");
     }
 
 }));
