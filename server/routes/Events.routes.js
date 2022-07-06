7837a8730984c85da6f01a7656f00ca6cefad924
Amara <70609663+RedSnoww12@users.noreply.github.com>
2022-07-06 16:11:36 +0200

    :warning: finir post one event

--- server/routes/Events.routes.js
+++ server/routes/Events.routes.js
@@ -1,16 +1,15 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import Event from './../models/EventModel.js';
-import events from './../data/event.js';
 
 const eventsRoute = express.Router();
 
 // INSERT BRUT DATA
-eventsRoute.post("/", asyncHandler(async (req, res) => {
-    await Event.remove({});
-    const eventsRes = await Event.insertMany(events);
-    res.json({eventsRes});
-}));
+// eventsRoute.post("/", asyncHandler(async (req, res) => {
+//     await Event.remove({});
+//     const eventsRes = await Event.insertMany(events);
+//     res.json({eventsRes});
+// }));
 
 
 // GET ALL EVENTS
@@ -30,28 +29,18 @@ eventsRoute.get("/:id", asyncHandler(async(req, res) => {
     }
 }));
 
-/*/ INSERT ONE EVENT
+// INSERT ONE EVENT
 eventsRoute.post("/", asyncHandler(async(req,res) =>{
     const { shoes_id, ticket_price, nb_ticket, starting_date, drawing_date } = req.body;
 
-    // const event = await Event.create({
-    //     shoes_id: shoes_id,
-    //     ticket_price: ticket_price,
-    //     nb_ticket: nb_ticket,
-    //     starting_date: starting_date,
-    //     drawing_date: drawing_date,
-    // });
-    await Event.remove({});
-    const event = await Event.insertMany(events);
+    const event = await Event.create({
+        
+    });
 
     if(event){
         res.status(201).json({
             _id: event.id,
-            shoes: event.shoes,
-            ticket_price: event.ticket_price,
-            nb_ticket: event.nb_ticket,
-            starting_date: event.starting_date,
-            drawing_date: event.drawing_date,
+            
         })
     }
     else {
@@ -59,6 +48,6 @@ eventsRoute.post("/", asyncHandler(async(req,res) =>{
         throw new Error("Invalid Data");
     }
 
-}));*/
+}));
 
 export default eventsRoute;
